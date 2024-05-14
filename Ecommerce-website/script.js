8c445886e44a69e7015eed31e49f4823bdd5babd
Anisfathima27 <122045826+Anisfathima27@users.noreply.github.com>
2024-05-14 09:14:01 +0530

    Add files via upload

---
+++ Ecommerce-website/script.js
@@ -0,0 +1,21 @@
+const bar=document.getElementById('bar');
+const close=document.getElementById('close');
+const nav=document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click',() =>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+}
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
